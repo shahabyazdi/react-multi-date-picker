7463d705be98cad81b541e0f5abe6defb9b40cb2
Shahab yazdi <sh.yazdi@gmail.com>
2020-09-03 13:14:32 +0430

    css updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-multi-date-picker/precache-manifest.400606a49753d9f541803f574cf396d3.js"
+  "/react-multi-date-picker/precache-manifest.7015f46a343b92a7fd2a017f0d4a574b.js"
 );
 
 self.addEventListener('message', (event) => {
