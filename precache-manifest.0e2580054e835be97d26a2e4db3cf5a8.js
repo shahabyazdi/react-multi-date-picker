c271a0a707a799ec326fcc54dd8611c308efcfdf
Shahab yazdi <sh.yazdi@gmail.com>
2020-11-04 18:46:53 +0330

    gh-pages

--- precache-manifest.0e2580054e835be97d26a2e4db3cf5a8.js
+++ precache-manifest.0e2580054e835be97d26a2e4db3cf5a8.js
@@ -1,35 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4055e1add2944baeb81cde9b0f04fcf",
+    "revision": "52b94fd33da9cdd40f9398ce4312b666",
     "url": "/react-multi-date-picker/index.html"
   },
   {
-    "revision": "519d5146a3ad831dfa79",
-    "url": "/react-multi-date-picker/static/css/2.c2e50c7e.chunk.css"
+    "revision": "0cb7236f76d688153a62",
+    "url": "/react-multi-date-picker/static/css/2.36a9f379.chunk.css"
   },
   {
-    "revision": "1d9b58a3ac03da714347",
-    "url": "/react-multi-date-picker/static/css/main.ce2b8607.chunk.css"
+    "revision": "3b770af4b44e21107d1e",
+    "url": "/react-multi-date-picker/static/css/main.aacd2269.chunk.css"
   },
   {
-    "revision": "519d5146a3ad831dfa79",
-    "url": "/react-multi-date-picker/static/js/2.4818630e.chunk.js"
+    "revision": "0cb7236f76d688153a62",
+    "url": "/react-multi-date-picker/static/js/2.b4dde513.chunk.js"
   },
   {
     "revision": "05ea761b7a5361472988577479d7ab28",
-    "url": "/react-multi-date-picker/static/js/2.4818630e.chunk.js.LICENSE.txt"
+    "url": "/react-multi-date-picker/static/js/2.b4dde513.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d9b58a3ac03da714347",
-    "url": "/react-multi-date-picker/static/js/main.3793b7e0.chunk.js"
+    "revision": "3b770af4b44e21107d1e",
+    "url": "/react-multi-date-picker/static/js/main.66de822f.chunk.js"
   },
   {
     "revision": "3aa1c54af65060506e40",
     "url": "/react-multi-date-picker/static/js/runtime-main.cc98768a.js"
-  },
-  {
-    "revision": "5f087b6ae8a6aa1729f245f0d18d0933",
-    "url": "/react-multi-date-picker/static/media/calendar.5f087b6a.svg"
   },
   {
     "revision": "07883e93734b98cae0f7b9c55d287250",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-multi-date-picker/precache-manifest.5ce63f630bded3265034ae8d351b58a5.js"
+  "/react-multi-date-picker/precache-manifest.0e2580054e835be97d26a2e4db3cf5a8.js"
 );
 
 self.addEventListener('message', (event) => {
