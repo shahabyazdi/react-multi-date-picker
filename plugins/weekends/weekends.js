37b79d4d4f99388aef8d9f1eabef4e48b2c41988
Shahab yazdi <sh.yazdi@gmail.com>
2021-01-03 17:57:33 +0330

    v1.9.3

--- plugins/weekends/weekends.js
+++ plugins/weekends/weekends.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react/cjs/react.development"
+import { useEffect, useRef } from "react"
 
 export default function Weekends({ state, setMapDays, weekends }) {
   const ref = useRef({})
