d4de804173ab35527e65cc3dc638496c8dfc16c4
Shahab yazdi <sh.yazdi@gmail.com>
2021-02-27 10:21:13 +0330

    v2.2.2

--- plugins/all.js
+++ plugins/all.js
@@ -1,6 +1,6 @@
 export { default as Settings } from "./all/settings/settings"
 export { default as DatePickerHeader } from "./all/date_picker_header/date_picker_header"
-export { default as MultiColors } from "./all/mutli_colors/multi_colors"
+export { default as MultiColors } from "./all/multi_colors/multi_colors"
 export { default as DatePanel } from "./all/date_panel/date_panel"
 export { default as Weekends } from "./all/weekends/weekends"
 export { default as Toolbar } from "./all/toolbar/toolbar"
--- rollup.config.js
+++ rollup.config.js
@@ -9,123 +9,137 @@ import url from "@rollup/plugin-url"
 import { terser } from "rollup-plugin-terser"
 
 const external = [
-    "react",
-    "react-dom",
-    "react-date-object",
-    "react-element-popper"
+  "react",
+  "react-dom",
+  "react-date-object",
+  "react-element-popper"
 ]
 
 const presets = [
-    "@babel/preset-react",
-    "@babel/preset-env"
+  "@babel/preset-react",
+  "@babel/preset-env"
 ]
 
 const globals = {
-    react: "React",
-    "react-date-object": "DateObject",
-    "react-element-popper": "ElementPopper"
+  react: "React",
+  "react-date-object": "DateObject",
+  "react-element-popper": "ElementPopper"
 }
 
 export default [
-    {
-        input: "src/index.js",
-        output: [
-            {
-                file: "build/index.js",
-                format: "esm",
-                plugins: [terser()],
-                exports: "named"
-            }
-        ],
-        external,
-        plugins: [
-            resolve(),
-            peerDepsExternal(),
-            babel({
-                exclude: /node_modules/,
-                presets
-            }),
-            commonjs(),
-            postcss({ plugins: [cssvariables()] }),
-            svgr(),
-            url()
-        ]
-    },
-    {
-        input: "src/index_browser.js",
-        output: [
-            {
-                file: "build/browser.min.js",
-                format: "umd",
-                plugins: [terser()],
-                name: "ReactMultiDatePicker",
-                exports: "named",
-                globals
-            }
-        ],
-        external,
-        plugins: [
-            resolve(),
-            peerDepsExternal(),
-            babel({
-                exclude: /node_modules/,
-                presets
-            }),
-            commonjs(),
-            postcss({ plugins: [cssvariables()] }),
-            svgr(),
-            url()
-        ]
-    },
-    {
-        input: "plugins/all.js",
-        output: [
-            {
-                file: "plugins/index.js",
-                format: "esm",
-                plugins: [terser()],
-                exports: "named"
-            }
-        ],
-        external,
-        plugins: [
-            resolve(),
-            peerDepsExternal(),
-            babel({
-                exclude: /node_modules/,
-                presets
-            }),
-            commonjs(),
-            postcss({ plugins: [cssvariables()] }),
-            svgr(),
-            url()
-        ]
-    },
-    {
-        input: "plugins/all.js",
-        output: [
-            {
-                file: "build/browser_plugins.min.js",
-                format: "umd",
-                plugins: [terser()],
-                name: "ReactMultiDatePickerPlugins",
-                exports: "named",
-                globals
-            }
-        ],
-        external,
-        plugins: [
-            resolve(),
-            peerDepsExternal(),
-            babel({
-                exclude: /node_modules/,
-                presets
-            }),
-            commonjs(),
-            postcss({ plugins: [cssvariables()] }),
-            svgr(),
-            url()
-        ]
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: "build/index.js",
+        format: "cjs",
+        plugins: [terser()],
+        exports: "named"
+      }
+    ],
+    external,
+    plugins: [
+      resolve(),
+      peerDepsExternal(),
+      babel({
+        exclude: /node_modules/,
+        presets
+      }),
+      commonjs(),
+      postcss({ plugins: [cssvariables()] }),
+      svgr(),
+      url()
+    ]
+  },
+  {
+    input: "src/index_browser.js",
+    output: [
+      {
+        file: "build/browser.min.js",
+        format: "umd",
+        plugins: [terser()],
+        name: "ReactMultiDatePicker",
+        exports: "named",
+        globals
+      }
+    ],
+    external,
+    plugins: [
+      resolve(),
+      peerDepsExternal(),
+      babel({
+        exclude: /node_modules/,
+        presets
+      }),
+      commonjs(),
+      postcss({ plugins: [cssvariables()] }),
+      svgr(),
+      url()
+    ]
+  },
+  {
+    input: "plugins/all.js",
+    output: [
+      {
+        file: "plugins/index.js",
+        format: "cjs",
+        plugins: [terser()],
+        exports: "named"
+      }
+    ],
+    external,
+    plugins: [
+      resolve(),
+      peerDepsExternal(),
+      babel({
+        exclude: /node_modules/,
+        presets
+      }),
+      commonjs(),
+      postcss({ plugins: [cssvariables()] }),
+      svgr(),
+      url()
+    ]
+  },
+  ...[
+    { path: "date_panel", name: "DatePanel" },
+    { path: "date_picker_header", name: "DatePickerHeader" },
+    { path: "multi_colors", name: "MultiColors" },
+    { path: "settings", name: "Settings" },
+    { path: "toolbar", name: "Toolbar" },
+    { path: "weekends", name: "Weekends" }
+  ].map(({ path, name }) => {
+    return {
+      input: `plugins/all/${path}/${path}.js`,
+      output: [
+        {
+          file: `plugins/${path}.js`,
+          format: "cjs",
+          plugins: [terser()],
+          exports: "named"
+        },
+        {
+          file: `build/${path}.browser.js`,
+          format: "umd",
+          plugins: [terser()],
+          name,
+          exports: "default",
+          globals
+        }
+      ],
+      external,
+      plugins: [
+        resolve(),
+        peerDepsExternal(),
+        babel({
+          exclude: /node_modules/,
+          presets
+        }),
+        commonjs(),
+        postcss({ plugins: [cssvariables()] }),
+        svgr(),
+        url()
+      ]
     }
-];
-
+  })
+]
