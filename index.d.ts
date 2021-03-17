c5f64e9bd9cfb2bd314a3ef1d2cd6a488dc09934
Your Name <you@example.com>
2021-03-17 19:13:40 +0330

    working on issue #24

--- index.d.ts
+++ index.d.ts
@@ -93,7 +93,7 @@ declare module "react-multi-date-picker" {
      *   ]}
      * />
      */
-    weekDays?: [string[]],
+    weekDays?: string[] | [string[]],
     /**
      * @see https://shahabyazdi.github.io/react-multi-date-picker/#custom-months-&-weekdays
      * @example
@@ -114,7 +114,7 @@ declare module "react-multi-date-picker" {
      *  ]}
      * />
      */
-    months?: [string[]],
+    months?: string[] | [string[]],
     /**
      * @example
      * <Calendar
