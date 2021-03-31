92e5b7008b366de0732610e5b6451e883a81606c
shahabyazdi <sh.yazdi@gmail.com>
2021-03-31 16:46:21 +0430

    v2.3.1

--- website/src/components/demo/quick_access.js
+++ website/src/components/demo/quick_access.js
@@ -35,13 +35,7 @@ const list = [
   },
   {
     name: "TypeScript",
-    path: "/typescript/",
-    list: [
-      {
-        name: "Usage",
-        path: "/typescript/#usage"
-      }
-    ]
+    path: "/typescript/"
   },
   {
     name: "Props",
@@ -156,6 +150,630 @@ const list = [
       {
         name: "Gregorian Calendar to Indian Calendar",
         path: "/date-object/#gregorian-calendar-to-indian-calendar"
+      }
+    ]
+  },
+  {
+    name: "Component with Children",
+    path: "/children/",
+    list: [
+      {
+        name: "Calendar With Children",
+        path: "/children/#calendar-with-children"
+      },
+      {
+        name: "DatePicker With Children",
+        path: "/children/#datepicker-with-children"
+      }
+    ]
+  },
+  {
+    name: "Multiple Mode",
+    path: "/multiple/",
+    list: [
+      {
+        name: "Multiple Mode",
+        path: "/multiple/#multiple-mode"
+      },
+      {
+        name: "DatePanel",
+        path: "/multiple/#datepanel"
+      },
+      {
+        name: "Sorting Dates",
+        path: "/multiple/#sorting-dates"
+      },
+      {
+        name: "Multiple Month Picker",
+        path: "/multiple/#multiple-month-picker"
+      },
+      {
+        name: "Multiple Year Picker",
+        path: "/multiple/#multiple-year-picker"
+      }
+    ]
+  },
+  {
+    name: "Range Mode",
+    path: "/range/",
+    list: [
+      {
+        name: "Range Mode",
+        path: "/range/#range-mode"
+      },
+      {
+        name: "DatePanel",
+        path: "/range/#datepanel"
+      },
+      {
+        name: "Displaying Each Days In Range",
+        path: "/range/#displaying-each-days-in-range"
+      },
+      {
+        name: "Range Month Picker",
+        path: "/range/#range-month-picker"
+      },
+      {
+        name: "Range Year Picker",
+        path: "/range/#range-year-picker"
+      }
+    ]
+  },
+  {
+    name: "Other Pickers",
+    path: "/other-pickers/",
+    list: [
+      {
+        name: "Time Picker",
+        path: "/other-pickers/#time-picker"
+      },
+      {
+        name: "Only Time Picker",
+        path: "/other-pickers/#only-time-picker"
+      },
+      {
+        name: "Only Time Picker Meridiem",
+        path: "/other-pickers/#only-time-picker-meridiem"
+      },
+      {
+        name: "Only Month Picker",
+        path: "/other-pickers/#only-month-picker"
+      },
+      {
+        name: "Only Month Picker With Different Format",
+        path: "/other-pickers/#only-month-picker-with-different-format"
+      },
+      {
+        name: "Only Year Picker",
+        path: "/other-pickers/#only-year-picker"
+      }
+    ]
+  },
+  {
+    name: "Multiple Months",
+    path: "/multiple-months/",
+    list: [
+      {
+        name: "Single Mode",
+        path: "/multiple-months/#single-mode"
+      },
+      {
+        name: "Multiple Mode",
+        path: "/multiple-months/#multiple-mode"
+      },
+      {
+        name: "Range Mode",
+        path: "/multiple-months/#range-mode"
+      },
+    ]
+  },
+  {
+    name: "Min & Max Date",
+    path: "/min-&-max-date/",
+    list: [
+      {
+        name: "Min & Max Date",
+        path: "/min-&-max-date/#min-&-max-date"
+      },
+      {
+        name: "String",
+        path: "/min-&-max-date/#string"
+      },
+      {
+        name: "Only MinDate",
+        path: "/min-&-max-date/#only-mindate"
+      },
+      {
+        name: "Only MaxDate",
+        path: "/min-&-max-date/#only-maxdate"
+      }
+    ]
+  },
+  {
+    name: "Events",
+    path: "/events/",
+    list: [
+      {
+        name: "On Change (single mode)",
+        path: "/events/#on-change-(single-mode)"
+      },
+      {
+        name: "On Change (multiple mode)",
+        path: "/events/#on-change-(multiple-mode)"
+      },
+      {
+        name: "onOpen",
+        path: "/events/#onopen"
+      },
+      {
+        name: "onClose",
+        path: "/events/#onclose"
+      },
+      {
+        name: "onPositionChange",
+        path: "/events/#onpositionchange"
+      }
+    ]
+  },
+  {
+    name: "Custom Months & WeekDays",
+    path: "/locales/",
+    list: [
+      {
+        name: "Week Days #1",
+        path: "/locales/#week-days-#1"
+      },
+      {
+        name: "Week Days #2",
+        path: "/locales/#week-days-#2"
+      },
+      {
+        name: "Months #1",
+        path: "/locales/#months-#1"
+      },
+      {
+        name: "Months #2",
+        path: "/locales/#months-#2"
+      },
+      {
+        name: "Format Months & WeekDays",
+        path: "/locales/#format-months-&-weekdays"
+      }
+    ]
+  },
+  {
+    name: "Types & Custom Input",
+    path: "/types/",
+    list: [
+      {
+        name: "Types",
+        path: "/types/#types"
+      },
+      {
+        name: "Input",
+        path: "/types/#input"
+      },
+      {
+        name: "Button",
+        path: "/types/#button"
+      },
+      {
+        name: "Input-Icon",
+        path: "/types/#input-icon"
+      },
+      {
+        name: "Icon",
+        path: "/types/#icon"
+      },
+      {
+        name: "Custom (function)",
+        path: "/types/#custom-(function)"
+      },
+      {
+        name: "Custom (component)",
+        path: "/types/#custom-(component)"
+      },
+      {
+        name: "Custom (input)",
+        path: "/types/#custom-(input)"
+      },
+      {
+        name: "Multiple & Range",
+        path: "/types/#multiple-&-range"
+      },
+      {
+        name: "Custom (multiple mode)",
+        path: "/types/#custom-(multiple-mode)"
+      },
+      {
+        name: "Custom (range mode)",
+        path: "/types/#custom-(range-mode)"
+      },
+    ]
+  },
+  {
+    name: "Customizing Calendar Days",
+    path: "/map-days/",
+    list: [
+      {
+        name: "Descriptions",
+        path: "/map-days/#descriptions"
+      },
+      {
+        name: "Styling Days",
+        path: "/map-days/#styling-days"
+      },
+      {
+        name: "Styling Weekends",
+        path: "/map-days/#styling-weekends"
+      },
+      {
+        name: "Custom Highlight",
+        path: "/map-days/#custom-highlight"
+      },
+      {
+        name: "Disabling Days",
+        path: "/map-days/#disabling-days"
+      },
+      {
+        name: "Hiding Days",
+        path: "/map-days/#hiding-days"
+      },
+      {
+        name: "Adding Tiltle to Days",
+        path: "map-days/#adding-tiltle-to-days"
+      }
+    ]
+  },
+  {
+    name: "Calendar Position",
+    path: "/positions/",
+    list: [
+      {
+        name: "Descriptions",
+        path: "/positions/#descriptions"
+      },
+      {
+        name: "Calendar Position",
+        path: "/positions/#calendar-position"
+      },
+      {
+        name: "Important Points",
+        path: "/positions/#important-points"
+      },
+      {
+        name: "Example",
+        path: "/positions/#example"
+      },
+      {
+        name: "Another Examples",
+        path: "/positions/#another-examples"
+      }
+    ]
+  },
+  {
+    name: "DatePicker & Calendar Ref",
+    path: "/ref/",
+    list: [
+      {
+        name: "Descriptions",
+        path: "/ref/#descriptions"
+      },
+      {
+        name: "Opene & Close Calendar By DatePicker Ref",
+        path: "/ref/#opene-&-close-calendar-by-datepicker-ref"
+      },
+      {
+        name: "Refresh Position",
+        path: "/ref/#refresh-position"
+      }
+    ]
+  },
+  {
+    name: "Appearance",
+    path: "/appearance/",
+    list: [
+      {
+        name: "Prime",
+        path: "/appearance/#prime"
+      },
+      {
+        name: "Mobile",
+        path: "/appearance/#mobile"
+      }
+    ]
+  },
+  {
+    name: "Colors & Backgrounds",
+    path: "/colors/",
+    list: [
+      {
+        name: "Green",
+        path: "/colors/#green"
+      },
+      {
+        name: "Red",
+        path: "/colors/#red"
+      },
+      {
+        name: "Yellow",
+        path: "/colors/#yellow"
+      },
+      {
+        name: "Purple",
+        path: "/colors/#purple"
+      },
+      {
+        name: "Teal",
+        path: "/colors/#teal"
+      },
+      {
+        name: "Background Dark",
+        path: "/colors/#background-dark"
+      },
+      {
+        name: "Background Gray",
+        path: "/colors/#background-gray"
+      },
+      {
+        name: "Background Brown",
+        path: "/colors/#background-brown"
+      }
+    ]
+  },
+  {
+    name: "ClassNames & Styles",
+    path: "/classes-&-styles/",
+    list: [
+      {
+        name: "Adding ClassName To Refrence Element (input)",
+        path: "/classes-&-styles/#adding-classname-to-refrence-element-(input)"
+      },
+      {
+        name: "Adding Style To Refrence Element (input)",
+        path: "/classes-&-styles/#adding-style-to-refrence-element-(input)"
+      },
+      {
+        name: "Adding ClassName To Popper Element (calendar)",
+        path: "/classes-&-styles/#adding-classname-to-popper-element-(calendar)"
+      },
+      {
+        name: "Adding Style To Arrow",
+        path: "/classes-&-styles/#adding-style-to-arrow"
+      },
+      {
+        name: "Adding ClassName To Arrow",
+        path: "/classes-&-styles/#adding-classname-to-arrow"
+      },
+      {
+        name: "Adding Style To DatePicker Container",
+        path: "/classes-&-styles/#adding-style-to-datepicker-container"
+      },
+      {
+        name: "Adding ClassName To DatePicker Container",
+        path: "/classes-&-styles/#adding-classname-to-datepicker-container"
+      }
+    ]
+  },
+  {
+    name: "Custom Arrow",
+    path: "/arrow/",
+    list: [
+      {
+        name: "Descriptions",
+        path: "/arrow/#descriptions"
+      },
+      {
+        name: "Disable Arrow",
+        path: "/arrow/#disable-arrow"
+      },
+      {
+        name: "Custom Arrow",
+        path: "/arrow/#custom-arrow"
+      }
+    ]
+  },
+  {
+    name: "Other Examples",
+    path: "/other-examples/",
+    list: [
+      {
+        name: "Animation",
+        path: "/other-examples/#animation"
+      },
+      {
+        name: "Other Days",
+        path: "/other-examples/#other-days"
+      },
+      {
+        name: "Disabling Scroll Sensitivity",
+        path: "/other-examples/#disabling-scroll-sensitivity"
+      },
+      {
+        name: "Hide On Scroll",
+        path: "/other-examples/#hide-on-scroll"
+      },
+      {
+        name: "Ignore Formatting",
+        path: "/other-examples/#ignore-formatting"
+      },
+      {
+        name: "Disable Virtual Keyboard",
+        path: "/other-examples/#disable-virtual-keyboard"
+      },
+      {
+        name: "Disable Editing",
+        path: "/other-examples/#disable-editing"
+      },
+      {
+        name: "Placeholder",
+        path: "/other-examples/#placeholder"
+      },
+      {
+        name: "Disable Year Picker",
+        path: "/other-examples/#disable-year-picker"
+      },
+      {
+        name: "Disable Month Picker",
+        path: "/other-examples/#disable-month-picker"
+      },
+      {
+        name: "Disabled Input",
+        path: "/other-examples/#disabled-input"
+      },
+      {
+        name: "Disabled Button",
+        path: "/other-examples/#disabled-button"
+      }
+    ]
+  },
+  {
+    name: "Plugins",
+    path: "/plugins/",
+    list: [
+      {
+        name: "Usage",
+        path: "/plugins/usage/",
+        list: [
+          {
+            name: "Availble Plugins",
+            path: "/plugins/usage/#availble-plugins"
+          },
+          {
+            name: "Positions",
+            path: "/plugins/usage/#positions"
+          },
+        ]
+      },
+      {
+        name: "Date Picker Header",
+        path: "/plugins/header/",
+        list: [
+          {
+            name: "Props",
+            path: "/plugins/header/#props"
+          },
+          {
+            name: "Sizes",
+            path: "/plugins/header/#sizes"
+          },
+          {
+            name: "Header Right",
+            path: "/plugins/header/#header-right"
+          },
+          {
+            name: "Header Left",
+            path: "/plugins/header/#header-left"
+          },
+          {
+            name: "English Calendar with Indian Header",
+            path: "/plugins/header/#english-calendar-with-indian-header"
+          },
+          {
+            name: "Styling Header",
+            path: "/plugins/header/#styling-header"
+          },
+        ]
+      },
+      {
+        name: "Date Panel",
+        path: "/plugins/panel/",
+        list: [
+          {
+            name: "Props",
+            path: "/plugins/panel/#props"
+          },
+          {
+            name: "Sort",
+            path: "/plugins/panel/#sort"
+          },
+          {
+            name: "Sort By Date",
+            path: "/plugins/panel/#sort-by-date"
+          },
+          {
+            name: "Position Left",
+            path: "/plugins/panel/#position-left"
+          },
+          {
+            name: "Without Remove Button",
+            path: "/plugins/panel/#without-remove-button"
+          },
+          {
+            name: "Date Click Listener",
+            path: "/plugins/panel/#date-click-listener"
+          }
+        ]
+      },
+      {
+        name: "Multi Colors",
+        path: "/plugins/colors/",
+        list: [
+          {
+            name: "Props",
+            path: "/plugins/colors/#props"
+          },
+          {
+            name: "Default Color",
+            path: "/plugins/colors/#default-color"
+          },
+          {
+            name: "With DatePanel",
+            path: "/plugins/colors/#with-datepanel"
+          },
+        ]
+      },
+      {
+        name: "Settings",
+        path: "/plugins/settings/",
+        list: [
+          {
+            name: "Props",
+            path: "/plugins/settings/#props"
+          },
+          {
+            name: "Settings Bottom",
+            path: "/plugins/settings/#settings-bottom"
+          },
+          {
+            name: "Custom Settings",
+            path: "/plugins/settings/#custom-settings"
+          },
+          {
+            name: "Time Picker",
+            path: "/plugins/settings/#time-picker"
+          }
+        ]
+      },
+      {
+        name: "Weekends",
+        path: "/plugins/weekends/",
+        list: [
+          {
+            name: "Props",
+            path: "/plugins/weekends/#props"
+          },
+          {
+            name: "Default Weekends",
+            path: "/plugins/weekends/#default-weekends"
+          },
+          {
+            name: "Weekends: gregorian",
+            path: "/plugins/weekends/#weekends:-gregorian"
+          },
+          {
+            name: "Weekends: persian",
+            path: "/plugins/weekends/#weekends:-persian"
+          },
+          {
+            name: "Weekends: custom",
+            path: "/plugins/weekends/#weekends:-custom"
+          },
+        ]
+      },
+      {
+        name: "Toolbar",
+        path: "/plugins/toolbar/"
       }
     ]
   }
