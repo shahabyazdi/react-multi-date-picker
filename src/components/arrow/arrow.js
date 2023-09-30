ec454fd35851926eabd701bd8ea8e520bf8c6391
Shahab <sh.yazdi@gmail.com>
2023-09-30 10:03:07 +0330

    button type added for Arrow component #225

--- src/components/arrow/arrow.js
+++ src/components/arrow/arrow.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Arrow({ direction, onClick, disabled }) {
   return (
     <button
+      type="button"
       className={`rmdp-arrow-container ${direction} ${
         disabled ? "disabled" : ""
       }`}
